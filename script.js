9734c4641bfb0bbafe2fe0050df4f79d6a97da61
Harsh Kumar <harshjangir8586@gmail.com>
2025-09-16 10:50:17 +0530

    Second Commit after implementing some baic JS

---
+++ script.js
@@ -0,0 +1,342 @@
+document.getElementsByClassName('nav_logo')[0].addEventListener('click', function() {
+  window.location.href = 'https://www.playstation.com/en-in/';
+});
+
+document.getElementsByClassName('nav_button')[0].addEventListener('click', function() {
+  window.location.href = 'https://my.account.sony.com/sonyacct/signin/?duid=0000000700090100e05e28452abe515b13dafe2b62d367eb8e94b37c83c0e8d6940d1f6fdd8b56cb&response_type=code&client_id=e4a62faf-4b87-4fea-8565-caaabb3ac918&scope=web%3Acore&access_type=offline&state=8a66d815f0495741dbc56dab72e9cfac349370d47da517807d8a02b7daec74ee&service_entity=urn%3Aservice-entity%3Apsn&ui=pr&smcid=web%3Apdc&redirect_uri=https%3A%2F%2Fweb.np.playstation.com%2Fapi%2Fsession%2Fv1%2Fsession%3Fredirect_uri%3Dhttps%253A%252F%252Fio.playstation.com%252Fcentral%252Fauth%252Flogin%253Flocale%253Den_IN%2526postSignInURL%253Dhttps%25253A%25252F%25252Fwww.playstation.com%25252Fen-in%25252F%2526cancelURL%253Dhttps%25253A%25252F%25252Fwww.playstation.com%25252Fen-in%25252F%26x-psn-app-ver%3D%2540sie-ppr-web-session%252Fsession%252Fv5.41.1&auth_ver=v3&error=login_required&error_code=4165&error_description=User+is+not+authenticated&no_captcha=true&cid=e14d7194-3e85-43d3-9381-603c636914c2#/signin/input/id';
+});
+
+document.getElementsByClassName('section1_button')[0].addEventListener('click', function() {
+  window.location.href = 'https://www.playstation.com/en-in/games/destiny-2/year-of-prophecy/#:~:text=The%20Year%20of%20Prophecy%20marks,(releasing%20December%202%2C%202025)';
+});
+
+document.getElementsByClassName('destiny2_img')[0].addEventListener('click', function() {
+  window.location.href = 'https://www.playstation.com/en-in/games/destiny-2/year-of-prophecy/#:~:text=The%20Year%20of%20Prophecy%20marks,(releasing%20December%202%2C%202025)';
+});
+
+document.getElementsByClassName('section2_button1')[0].addEventListener('click', function() {
+  window.location.href = 'https://www.playstation.com/en-in/ps5/';
+});
+
+document.getElementsByClassName('header')[0].addEventListener('click', function() {
+  window.location.href = 'https://www.sony.com/en/'
+});
+
+document.getElementsByClassName('section2_button2')[0].addEventListener('click', function() {
+  window.location.href = 'https://www.playstation.com/en-in/ps5/';
+});
+
+document.getElementsByClassName('section5_button1')[0].addEventListener('click', function() {
+  window.location.href = 'https://www.playstation.com/en-in/30th-anniversary/';
+});
+
+document.getElementsByClassName('section5_button2')[0].addEventListener('click', function() {
+  window.location.href = 'https://www.playstation.com/en-in/playstation-history/1994-ps-one/';
+});
+
+document.getElementsByClassName('section6_button')[0].addEventListener('click', function() {
+  window.location.href = 'https://www.playstation.com/en-in/editorial/this-month-on-playstation/';
+});
+
+document.getElementsByClassName('section7_button')[0].addEventListener('click', function() {
+  window.location.href = 'https://store.playstation.com/en-in/pages/deals';
+});
+
+document.getElementById('s1_img1').addEventListener('click', function() {
+  window.location.href = 'https://www.playstation.com/en-in/games/mafia-the-old-country/';
+});
+
+document.getElementById('s1_img2').addEventListener('click', function() {
+  window.location.href = 'https://www.playstation.com/en-in/games/battlefield-6/';
+});
+
+document.getElementById('s1_img3').addEventListener('click', function() {
+  window.location.href = 'https://store.playstation.com/en-in/category/05f796ea-3420-4946-9d14-b931c8a8f41b/1';
+});
+
+document.getElementById('s1_img4').addEventListener('click', function() {
+  window.location.href = 'https://www.playstation.com/en-in/accessories/flexstrike-wireless-fight-stick/';
+});
+
+document.getElementById('s1_img5').addEventListener('click', function() {
+  window.location.href = 'https://www.playstation.com/en-in/games/destiny-2/year-of-prophecy/#:~:text=The%20Year%20of%20Prophecy%20marks,(releasing%20December%202%2C%202025)';
+});
+
+document.getElementById('s3_img1').addEventListener('click', function() {
+  window.location.href = 'https://www.playstation.com/en-in/games/death-stranding-2-on-the-beach/';
+});
+
+document.getElementById('s3_img2').addEventListener('click', function() {
+  window.location.href = 'https://www.playstation.com/en-in/games/astro-bot/';
+});
+
+document.getElementById('s3_img3').addEventListener('click', function() {
+  window.location.href = 'https://www.playstation.com/en-in/games/tony-hawks-pro-skater-3-4/';
+});
+
+document.getElementById('s3_img4').addEventListener('click', function() {
+  window.location.href = 'https://www.playstation.com/en-in/games/mafia-the-old-country/';
+});
+
+document.getElementById('s3_img5').addEventListener('click', function() {
+  window.location.href = 'https://www.playstation.com/en-in/games/elden-ring-nightreign/';
+});
+
+document.getElementById('s3_img6').addEventListener('click', function() {
+  window.location.href = 'https://www.playstation.com/en-in/games/the-elder-scrolls-iv-oblivion-remastered/';
+});
+
+document.getElementById('s3_img7').addEventListener('click', function() {
+  window.location.href = 'https://www.playstation.com/en-in/games/clair-obscur--expedition-33/';
+});
+
+document.getElementById('s3_img8').addEventListener('click', function() {
+  window.location.href = 'https://www.playstation.com/en-in/games/forza-horizon-5/';
+});
+
+document.getElementById('s3_img9').addEventListener('click', function() {
+  window.location.href = 'https://www.playstation.com/en-in/games/call-of-duty-black-ops-6/';
+});
+
+document.getElementById('s3_img10').addEventListener('click', function() {
+  window.location.href = 'https://www.playstation.com/en-in/games/monster-hunter-wilds/';
+});
+
+document.getElementById('s3_img11').addEventListener('click', function() {
+  window.location.href = 'https://www.playstation.com/en-in/games/black-myth-wukong/';
+});
+
+document.getElementById('s3_img12').addEventListener('click', function() {
+  window.location.href = 'https://www.playstation.com/en-in/games/helldivers-2/';
+});
+
+document.getElementById('s6_img1').addEventListener('click', function() {
+  window.location.href = 'https://www.playstation.com/en-in/editorial/this-month-on-playstation/';
+});
+
+document.getElementById('s6_img2').addEventListener('click', function() {
+  window.location.href = 'https://www.playstation.com/en-in/editorial/this-month-on-playstation/#ps-indies';
+});
+
+document.getElementById('s6_img3').addEventListener('click', function() {
+  window.location.href = 'https://www.playstation.com/en-in/games/helldivers-2/helldivers-2-update-summary/';
+});
+
+document.getElementsByClassName('store_img')[0].addEventListener('click', function() {
+  window.location.href = 'https://store.playstation.com/en-in/pages/deals/';
+});
+
+document.getElementById('s8_i1').addEventListener('click', function() {
+  window.location.href = 'https://blog.playstation.com/2025/08/14/midnight-murder-club-jumps-out-of-the-early-access-shadows-today/';
+});
+
+document.getElementById('s8_i2').addEventListener('click', function() {
+  window.location.href = 'https://blog.playstation.com/2025/08/13/playstation-plus-game-catalog-for-august-mortal-kombat-1-marvels-spider-man-sword-of-the-sea-earth-defense-force-6-and-more/';
+});
+
+document.getElementById('s8_i3').addEventListener('click', function() {
+  window.location.href = 'https://blog.playstation.com/2025/08/12/15-must-play-indies-to-mark-15-years-of-playstation-plus/';
+});
+
+document.getElementById('s9_1').addEventListener('click', function() {
+  window.location.href = 'https://x.com/PlayStationIN';
+});
+
+document.getElementById('s9_2').addEventListener('click', function() {
+  window.location.href = 'https://www.facebook.com/SonyPlaystationIndia';
+});
+
+document.getElementById('s9_3').addEventListener('click', function() {
+  window.location.href = 'https://www.instagram.com/playstation/';
+});
+
+document.getElementById('s9_4').addEventListener('click', function() {
+  window.location.href = 'https://www.youtube.com/channel/UClcQ90LedRcM_HFR-mZsENg';
+});
+
+document.getElementById('last1_1').addEventListener('click', function() {
+  window.location.href = 'https://www.playstation.com/en-in/corporate/about-us/';
+});
+
+document.getElementById('last1_2').addEventListener('click', function() {
+  window.location.href = 'https://careers.playstation.com/';
+});
+
+document.getElementById('last1_3').addEventListener('click', function() {
+  window.location.href = 'https://www.playstation.com/en-in/corporate/playstation-studios/';
+});
+
+document.getElementById('last1_4').addEventListener('click', function() {
+  window.location.href = 'https://www.playstation.com/en-in/playstation-productions/';
+});
+
+document.getElementById('last1_5').addEventListener('click', function() {
+  window.location.href = 'https://sonyinteractive.com/en/';
+});
+
+document.getElementById('last1_6').addEventListener('click', function() {
+  window.location.href = 'https://www.playstation.com/en-in/playstation-history/1994-ps-one/';
+});
+
+document.getElementById('last2_1').addEventListener('click', function() {
+  window.location.href = 'https://www.playstation.com/en-in/ps5/';
+});
+
+document.getElementById('last2_2').addEventListener('click', function() {
+  window.location.href = 'https://www.playstation.com/en-in/ps4/';
+});
+
+document.getElementById('last2_3').addEventListener('click', function() {
+  window.location.href = 'https://www.playstation.com/en-in/ps-vr2/';
+});
+
+document.getElementById('last2_4').addEventListener('click', function() {
+  window.location.href = 'https://www.playstation.com/en-in/ps-plus/';
+});
+
+document.getElementById('last2_5').addEventListener('click', function() {
+  window.location.href = 'https://www.playstation.com/en-in/accessories/';
+});
+
+document.getElementById('last2_6').addEventListener('click', function() {
+  window.location.href = 'https://www.playstation.com/en-in/ps5/games/';
+});
+
+document.getElementById('last3_1').addEventListener('click', function() {
+  window.location.href = 'https://www.playstation.com/en-in/corporate/playstation-and-the-environment/';
+});
+
+document.getElementById('last3_2').addEventListener('click', function() {
+  window.location.href = 'https://www.playstation.com/en-in/accessibility/';
+});
+
+document.getElementById('last3_3').addEventListener('click', function() {
+  window.location.href = 'https://www.playstation.com/en-in/privacy-security-safety/';
+});
+
+document.getElementById('last3_4').addEventListener('click', function() {
+  window.location.href = 'https://sonyinteractive.com/en/impact/diversity-and-beyond/';
+});
+
+document.getElementById('last4_1').addEventListener('click', function() {
+  window.location.href = 'https://www.playstation.com/en-in/support/';
+});
+
+document.getElementById('last4_2').addEventListener('click', function() {
+  window.location.href = 'https://www.playstation.com/en-in/support/safety/';
+});
+
+document.getElementById('last4_3').addEventListener('click', function() {
+  window.location.href = 'https://status.playstation.com/';
+});
+
+document.getElementById('last4_4').addEventListener('click', function() {
+  window.location.href = 'https://repairs.playstation.com/s/?locale=en-in&language=en_GB';
+});
+
+document.getElementById('last4_5').addEventListener('click', function() {
+  window.location.href = 'https://www.playstation.com/en-in/support/account/password-reset-psn/';
+});
+
+document.getElementById('last5_1').addEventListener('click', function() {
+  window.location.href = 'https://www.playstation.com/en-in/legal/psn-terms-of-service/';
+});
+
+document.getElementById('last5_2').addEventListener('click', function() {
+  window.location.href = 'https://www.playstation.com/en-in/legal/playstation-store-cancellation-policy/';
+});
+
+document.getElementById('last5_3').addEventListener('click', function() {
+  window.location.href = 'https://www.playstation.com/en-in/support/account/age-ratings-playstation/';
+});
+
+document.getElementById('last5_4').addEventListener('click', function() {
+  window.location.href = 'https://www.playstation.com/en-in/legal/health-warning/';
+});
+
+document.getElementById('last5_5').addEventListener('click', function() {
+  window.location.href = 'https://partners.playstation.net/';
+});
+
+document.getElementById('last5_6').addEventListener('click', function() {
+  window.location.href = 'https://www.playstation.com/en-in/editorial/playstation-ultimate-gaming-glossary/';
+});
+
+document.getElementById('last5_7').addEventListener('click', function() {
+  window.location.href = 'https://www.playstation.com/en-in/official-licensing-program/';
+});
+
+document.getElementById('last_img_1').addEventListener('click', function() {
+  window.location.href = 'https://www.facebook.com/SonyPlaystationIndia';
+});
+
+document.getElementById('last_img_2').addEventListener('click', function() {
+  window.location.href = 'https://x.com/PlayStationIN';
+});
+
+document.getElementById('last_img_3').addEventListener('click', function() {
+  window.location.href = 'https://www.instagram.com/playstationin/';
+});
+
+document.getElementById('last_img_4').addEventListener('click', function() {
+  window.location.href = 'https://www.youtube.com/@playstationin/videos';
+});
+
+document.getElementById('last6_1').addEventListener('click', function() {
+  window.location.href = 'https://apps.apple.com/us/app/playstation-app/id410896080';
+});
+
+document.getElementById('last6_2').addEventListener('click', function() {
+  window.location.href = 'https://play.google.com/store/apps/details?id=com.scee.psxandroid&utm_source=pdcexploreapp';
+});
+
+document.getElementsByClassName('more_info')[0].addEventListener('click', function() {
+  window.location.href = 'https://www.playstation.com/en-in/legal/copyright-and-trademark-notice/';
+});
+
+document.getElementById('footer_1').addEventListener('click', function() {
+  window.location.href = 'https://www.playstation.com/en-in/legal/';
+});
+
+document.getElementById('footer_2').addEventListener('click', function() {
+  window.location.href = 'https://www.playstation.com/en-in/legal/privacy-policy/';
+});
+
+document.getElementById('footer_3').addEventListener('click', function() {
+  window.location.href = 'https://www.playstation.com/en-in/legal/website-terms-of-use/';
+});
+
+document.getElementById('footer_4').addEventListener('click', function() {
+  window.location.href = 'https://www.playstation.com/en-in/site-map/';
+});
+
+document.getElementById('footer_5').addEventListener('click', function() {
+  window.location.href = 'https://www.playstation.com/en-in/legal/cookies/';
+});
+
+document.getElementById('footer_6').addEventListener('click', function() {
+  window.location.href = 'https://www.playstation.com/en-in/legal/software-usage-terms/';
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    const searchLogo = document.querySelector('.nav_search_logo');
+    const searchBarContainer = document.getElementById('search-bar-container');
+    const searchBar = document.getElementById('search-bar');
+
+    searchLogo.addEventListener('click', function (e) {
+        e.stopPropagation();
+        searchBarContainer.style.display = 
+            searchBarContainer.style.display === 'block' ? 'none' : 'block';
+        if (searchBarContainer.style.display === 'block') {
+            searchBar.focus();
+        }
+    });
+
+    document.addEventListener('click', function () {
+        searchBarContainer.style.display = 'none';
+    });
+
+    searchBarContainer.addEventListener('click', function (e) {
+        e.stopPropagation();
+    });
+});
